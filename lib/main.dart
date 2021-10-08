3e21ade4d0c36f068079f78905748ad6b5df1854
Ivy Hung <ivyhungtaiwan@gmail.com>
2021-10-08 22:48:27 +0800

    feat: install intl and formate dates

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import './transaction.dart';
+import 'package:intl/intl.dart';
 
 void main() => runApp(MyApp());
 
@@ -71,7 +72,7 @@ class MyHomePage extends StatelessWidget {
                           ),
                         ),
                         Text(
-                          tx.date.toString(),
+                          DateFormat.yMMMd().format(tx.date),
                           style: TextStyle(
                             color: Colors.grey,
                           )

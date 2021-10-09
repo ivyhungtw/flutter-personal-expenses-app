c642ca73cdd6f6a5da02bb8118a208155d17c3d4
Ivy Hung <ivyhungtaiwan@gmail.com>
2021-10-09 16:47:42 +0800

    feat: config input & output style

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -10,6 +10,20 @@ class NewTransaction extends StatelessWidget {
 
   NewTransaction(this.addTx);
 
+  void submitData() {
+    final enteredTitle = titleController.text;
+    final enteredAmount = double.parse(amountController.text);
+    
+    if (enteredTitle.isEmpty || enteredAmount <= 0) {
+      return;
+    }
+
+    addTx(
+      enteredTitle, 
+      enteredAmount
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -20,26 +34,25 @@ class NewTransaction extends StatelessWidget {
                 TextField(
                   decoration: InputDecoration(labelText: 'Title'),
                   controller: titleController,
+                  onSubmitted: (_) => submitData()
                   // onChanged: (val) => titleInput = val,
                 ),
                 TextField(
                   decoration: InputDecoration(labelText: 'Amount'),
                   controller: amountController,
+                  keyboardType: TextInputType.numberWithOptions(decimal: true),
+                  onSubmitted: (_) => submitData(),
                   // onChanged: (val) => amountInput = val,
                 ),
                 TextButton(
                   child: Text(
-                    'Add Transaction',
-                    style: TextStyle(
-                    color: Colors.purple,
-                  ),
+                      'Add Transaction',
+                      style: TextStyle(
+                      color: Colors.purple,
+                    ),
                   ), 
-                  onPressed: () {
-                    addTx(
-                      titleController.text, 
-                      double.parse(amountController.text)
-                    );
-                  }, )
+                  onPressed: submitData, 
+                )
               ]
             ),
           );
